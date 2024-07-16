1328879574ef409383e09e828af74885a0b7847e
vciocoiu <vciocoiu@adobe.com>
2024-07-16 15:26:26 +0300

    Add layout for all screens

--- blocks/media-gallery/media-gallery.js
+++ blocks/media-gallery/media-gallery.js
@@ -1,5 +1,23 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+    // modify card structure to have a card-content div
+    for(let idx = 0; idx < block.children.length; ++idx) {
+        const child = block.children[idx];
+        child.classList.add(`card-${idx}`);
 
+        const cardContent = document.createElement('div');
+        cardContent.classList.add('card-content');
+
+        const cardTitle = child.children[0].children[0];
+        const cardDescription = child.children[0].children[1];
+
+        cardTitle.classList.add('card-title');
+        cardDescription.classList.add('card-description');
+
+        cardContent.appendChild(cardTitle);
+        cardContent.appendChild(cardDescription);
+
+        child.children[0].appendChild(cardContent);
+    }
 }
