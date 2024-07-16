7e7d2f0184c6e1cf5f5acf2a740da18c2a4df223
vciocoiu <vciocoiu@adobe.com>
2024-07-16 17:35:33 +0300

    Add start video buttons

--- blocks/media-gallery/media-gallery.js
+++ blocks/media-gallery/media-gallery.js
@@ -4,11 +4,20 @@ export default function decorate(block) {
     // modify card structure to have a card-content div
     for(let idx = 0; idx < block.children.length; ++idx) {
         const child = block.children[idx];
+        child.classList.add('card');
         child.classList.add(`card-${idx}`);
 
         const cardContent = document.createElement('div');
         cardContent.classList.add('card-content');
 
+        console.log(block.classList);
+        if(block.classList.contains('alternate-2-1')) {
+            const startButton = document.createElement('button');
+            startButton.classList.add('start-button');
+            startButton.innerHTML = "<svg aria-hidden='true' class='svg' fill='currentColor' height='100%' viewBox='0 0 24 24' width='100%' xmlns='http://www.w3.org/2000/svg'><path d='M6 1.773l15 10.23L6 22.226z'></path></svg>";
+            cardContent.appendChild(startButton);
+        }
+        
         const cardTitle = child.children[0].children[0];
         const cardDescription = child.children[0].children[1];
 
