63949b6d53285839d2c92059d559ad779b3f7449
anu13297 <amallempati@esri.com>
2024-07-25 15:11:02 -0400

    esri text pattern - indentation error fix

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,20 +1,18 @@
 import { ul, li } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
+  block.classList.add('esri-text');
+  const list = ul(...[...block.children].map((row) => li(...[...row.children].map((divElement) => {
+    if (
+      divElement.children.length === 1
+      && divElement.querySelector('picture')
+    ) {
+      divElement.className = 'esritext-image';
+    } else divElement.className = 'esritext-body';
 
-    block.classList.add('esri-text');
-
-    const list = ul(...[...block.children].map((row) => li(...[...row.children].map((divElement) => {
-        if (
-            divElement.children.length === 1
-            && divElement.querySelector('picture')
-        ) {
-            divElement.className = 'esritext-image';
-        } else divElement.className = 'esritext-body';
-
-        return divElement;
+    return divElement;
     }))));
-    
-    block.textContent = '';
-    block.append(list);
+
+  block.textContent = '';
+  block.append(list);
 }
