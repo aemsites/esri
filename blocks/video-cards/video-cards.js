530d84d829bb9a111677ffd3bc8acba7f9269919
vciocoiu <vciocoiu@adobe.com>
2024-07-29 16:54:11 +0300

    Add hover animation and video lightbox

--- blocks/video-cards/video-cards.js
+++ blocks/video-cards/video-cards.js
@@ -1,5 +1,5 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { p } from '../../scripts/dom-helpers.js';
+import { p, button, domEl } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   block.querySelectorAll('img').forEach((img) => {
@@ -17,4 +17,27 @@ export default function decorate(block) {
     ul.children[2].replaceChildren(p({ class: 'video-card-description' }, ul.children[2].textContent));
   });
 
+
+  const startButton = button(domEl('calcite-icon', {
+    icon: 'play-f',
+    scale: 's',
+  }));
+
+  [...block.children].forEach((child) => {
+    child.children[0].append(startButton.cloneNode(true));
+  });
+
+  
+
+  [...block.children].forEach((child) => {
+    // wrap card in anchor tag
+    const anchor = child.querySelector('a');
+
+    if (anchor) {
+      anchor.parentElement.parentElement.removeChild(anchor.parentElement);
+      anchor.textContent = '';
+      anchor.appendChild(child);
+      block.appendChild(anchor);
+    }
+  });
 };
