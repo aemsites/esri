864b4164329fc6edb913358ab849adf95197cc3d
Tim White <tim_white@esri.com>
2024-08-01 11:27:22 -0700

    feat: Add global nav scripts and JSON to header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -91,6 +91,15 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
+  // Global nav
+  const json = document.createElement('script');
+  json.appendChild(document.createTextNode('window.gnav_jsonPath = 20220-nav-config.25.json'));
+  block.appendChild(json);
+
+  const script = document.createElement('script');
+  script.src = 'https://webapps-cdn.esri.com/CDN/components/global-nav/js/gn.js';
+  block.appendChild(script);
+  
   // load nav as fragment
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
