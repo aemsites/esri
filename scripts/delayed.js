ccdc30a481c6219225a92bac773bae5ec16e3c5a
Tim White <tim_white@esri.com>
2024-08-01 13:18:45 -0700

    fix: update to append to head

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -10,4 +10,4 @@ sampleRUM('cwv');
 const script = document.createElement('script');
 script.src = 'https://assets.adobedtm.com/launch-ENd3e1f5f2e5b44e4f8c9e7b0c4a2f8b3.min.js';
 script.async = true;
-document.body.appendChild(script);
+document.head.appendChild(script);
