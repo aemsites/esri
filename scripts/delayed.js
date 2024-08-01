dfeaa8676b430ec9ad7241f673d361016a6de08d
Tim White <tim_white@esri.com>
2024-08-01 13:11:38 -0700

    fix: remove console.log

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -11,4 +11,3 @@ const script = document.createElement('script');
 script.src = 'https://assets.adobedtm.com/launch-ENd3e1f5f2e5b44e4f8c9e7b0c4a2f8b3.min.js';
 script.async = true;
 document.body.appendChild(script);
-console.log('Launch loaded');
