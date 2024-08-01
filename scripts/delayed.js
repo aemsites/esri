7aa603cae45882a7c0ed491b5614b9760ed42912
Tim White <tim_white@esri.com>
2024-08-01 13:10:05 -0700

    feat: move Launch to delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,10 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// Launch script
+const script = document.createElement('script');
+script.src = 'https://assets.adobedtm.com/launch-ENd3e1f5f2e5b44e4f8c9e7b0c4a2f8b3.min.js';
+script.async = true;
+document.body.appendChild(script);
+console.log('Launch loaded');
