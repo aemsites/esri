6dba57c1cf67168985bfdbc25bbd32d327ce1b35
Joe-Savella <josephsavella@yahoo.com>
2024-08-02 13:57:38 -0700

    leverage block param

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,17 +1,15 @@
 export default function decorate(block) {
-  const heroBlock = document.querySelector('.hero');
   const videoElement = document.createElement('video');
   const videoSrc = document.createElement('source');
-  const videoAssets = heroBlock.querySelectorAll('a');
+  const videoAssets = block.querySelectorAll('a');
 
   videoSrc.setAttribute('type', 'video/mp4');
   videoElement.setAttribute('type', 'video/mp4');
   videoElement.setAttribute('muted', '');
-  // videoSrc.setAttribute('src', videoAssets[1].getAttribute('title'));
-  videoSrc.setAttribute('src', 'https://www.esri.com/content/dam/esrisites/en-us/about/about/about-2022/updates-10/about-overview-banner.mp4');
+  videoSrc.setAttribute('src', videoAssets[1].getAttribute('title'));
 
   if (videoElement) videoElement.append(videoSrc);
-  if (videoAssets) heroBlock.prepend(videoElement);
+  if (videoAssets) block.prepend(videoElement);
 
   videoElement.play();
 }
