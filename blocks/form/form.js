d856a290d8e6a8388d74432148f8561faa00085d
vciocoiu <vciocoiu@adobe.com>
2024-08-12 16:20:15 +0300

    Add form props

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -2,7 +2,6 @@ import { div } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   const divId = 'one-form-target-div';
-  const formId = 'vladtestform';
 
   block.appendChild(div({ id: divId }));
 
@@ -11,13 +10,47 @@ export default function decorate(block) {
 
   window.initOneForm(divId, {
     divId,
-    // mode: 'custom-form',
+    aemFieldServiceBasePath: '/content/experience\u002Dfragments/esri\u002Dsites/en\u002Dus/site\u002Dsettings/one\u002Dform\u002Dadmin/master',
+    aemEditMode: 'false',
     mode: 'basic-progressive-form',
-    darkMode: true,
-    formName: formId,
-    thankYouFormType: 'mql-optional-web-app',
-    // customFormConfig: 'https://assets.esri.com/content/experience-fragments/esri-sites/en-us/forms/testing_sandbox/daniel-test/master',
+    formName: '2854884_Imagery Organic Traffic Campaign: Workflow to Asset LPs \u002D Contact Sales',
+    formOpensInAModal: '',
+    modalTitle: '',
+    formModalLookup: '2854884_Imagery Organic Traffic Campaign: Workflow to Asset LPs \u002D Contact Sales',
+    leftAligned: '',
+    darkMode: 'true',
+    transparentBackground: '',
+    pardotHandler: 'https://go.esri.com/l/82202/2022\u002D05\u002D31/pnykw9',
+    organicSfId: '7015x000001PKGoAAO',
+    isolation: '',
+    disablePersonalization: '' === 'true',
+    inlineThankYouPage: '',
+    thankYouFormType: 'high',
+    thankYouBannerImage: '',
+    thankYouAssetTitle: '',
+    thankYouAssetType: 'Brochure',
+    thankYouAssetPath: '',
+    thankYouListName: '',
+    thankYouHeader: '',
+    thankYouMessage: '',
+    mqlComment: 'Please review the \x27What Prompted Your Interest?\x27 field for follow up.',
     mqlBehavior: 'default',
-    gdprMode: 'standard',
+    mqlFormHandler: '',
+    gdprMode: '',
+    showEventConsentCheckBoxes: '' === 'true',
+    marketingConsentRequired: '',
+    marketingConsentRequiredMessage: '',
+    customMarketingConsentLabel: '',
+    customMarketingConsentRequiredMessage: '',
+    customContactConsentLabel: '',
+    customContactConsentRequiredMessage: '',
+    thankyouPageUrl: '',
+    thankyouPageParams: '',
+    sendEmail: '' === 'true',
+    emailTo: '',
+    emailCc: '',
+    emailBcc: '',
+    emailSubject: '',
+    emailBody: '',
   });
 }
