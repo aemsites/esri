5d86a149d80596bc6728609425ff6645e9e508d2
anu13297 <amallempati@esri.com>
2024-08-16 11:02:13 -0400

    #95-Card Standard Variation

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -23,6 +23,31 @@ export default function decorate(block) {
     });
   };
 
+  const processStandardCard = (div) => {
+    if (!block.classList.contains('standard')) {
+      return;
+    }
+    block.classList.add('cardsperrow');
+    const anchorEl = div.querySelector('a');
+    const cardBodyContent = domEl('div', { class: 'card-body-content' });
+    if (anchorEl) {
+      anchorEl.replaceChildren(cardBodyContent);
+      div.append(anchorEl);
+    } else {
+      div.append(cardBodyContent);
+    }
+    div.querySelectorAll('p').forEach((p) => { if (p.textContent === '') p.remove(); });
+    [...div.querySelectorAll('.cards-card-body > :not(.card-body-content, a)')].forEach((el) => {
+      cardBodyContent.append(el);
+      if (el.tagName === 'P' && el.children.length === 0 && el.parentNode.firstElementChild === el) {
+        el.classList.add('overlay-text');
+      }
+    });
+    const pictureEl = div.querySelector('picture').closest('p');
+    const overlayTextEl = div.querySelector('.overlay-text');
+    if (overlayTextEl) pictureEl.append(overlayTextEl);
+  };
+
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -32,6 +57,7 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else { div.className = 'cards-card-body'; }
       processSimpleCard(div);
+      processStandardCard(div);
     });
     ul.append(li);
   });
