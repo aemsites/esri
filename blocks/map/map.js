626043f50fea6720c244e5e63cf77d2b9b019a25
Abel Simon <asimon@adobe.com>
2024-08-16 14:37:21 +0200

    chore: different threshold for the observer

--- blocks/map/map.js
+++ blocks/map/map.js
@@ -74,7 +74,7 @@ export default async function decorate(block) {
       }
     });
     observer.disconnect();
-  }, { rootMargin: '100px' });
+  }, { threshold: 0.5 });
 
   observer.observe(frameWrapper);
 
