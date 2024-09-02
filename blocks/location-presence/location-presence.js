5827f5cde8500a39c320575041d2ced281d3ad1f
Abel Simon <asimon@adobe.com>
2024-09-02 11:47:47 +0200

    chore: add styling for location presence block

--- blocks/location-presence/location-presence.js
+++ blocks/location-presence/location-presence.js
@@ -1,34 +1,31 @@
 import {
-    domEl,
-    div,
-    a,
-    span,
-    h2,
-    p,
-    calciteButton
-  } from '../../scripts/dom-helpers.js';
-  
+  div,
+  h2,
+  p,
+  calciteButton,
+} from '../../scripts/dom-helpers.js';
 
 function getDOMElements(hText, pText, buttonText, buttonHref) {
-  
-  const heading = h2({class: "location-presence-heading"}, hText)
-  const text = p({class: "location-presence-text"}, pText)
-  const findYourEsriOfficeButton = calciteButton({class: "location-presence-button", href: buttonHref}, buttonText)
-  const contentWrapper = div({class: "location-content-container"}, heading, text, findYourEsriOfficeButton)
-  return contentWrapper
+  const heading = h2({ class: 'location-presence-heading' }, hText);
+  const text = p({ class: 'location-presence-subtitle' }, pText);
+  const findYourEsriOfficeButton = calciteButton({ class: 'location-presence-button', scale: 'l', href: buttonHref }, buttonText);
+  const contentWrapper = div({ class: 'location-content-container' }, heading, text, findYourEsriOfficeButton);
+  return contentWrapper;
 }
-export default function decorate() {
-    const locationPresenceBlock = document.querySelector('.location-presence-wrapper > div > div > div');
-    
-    const [ heading, text, button ] = locationPresenceBlock.children
+export default function decorate(block) {
+  const locationPresenceBlock = document.querySelector('.location-presence-wrapper > div > div > div');
 
-    const elements = getDOMElements(heading.textContent, text.textContent, button.children[0].textContent, button.children[0].href)
+  const [heading, text, button] = locationPresenceBlock.children;
 
-    console.log('these are the elements, ', heading, text, button)
-    console.log('these are computed elements', elements)
+  const elements = getDOMElements(
+    heading.textContent,
+    text.textContent,
+    button.children[0].textContent,
+    button.children[0].href,
+  );
 
-    locationPresenceBlock.innerHTML = ""
-    // locationPresenceBlock.classList.add("")
+  locationPresenceBlock.innerHTML = '';
+  block.classList.add('calcite-mode-dark');
 
-    locationPresenceBlock.appendChild(elements)
-  }
+  locationPresenceBlock.appendChild(elements);
+}
